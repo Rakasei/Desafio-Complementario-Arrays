81a513b01f37105500c1dcc7bdee4dd050dc1f58
Rodrigo Ezequiel Ruiz <rodrigo.ezequiel.ruiz@gmail.com>
2022-07-25 23:45:03 -0300

    Subida al servidor de github

---
+++ js/app.js
@@ -0,0 +1,13 @@
+alert("Bienvenido al Servicio de Cálculo de Impuestos de Productos Adquiridos en la Plataforma de Steam (SCIPAPS")
+
+function pais de residencia
+const pais = prompt("¿En que país vivís?")
+
+if (pais = argentina){    
+    alert("Perfecto, ingresá el valor del producto que querés adquirir y SCIPAPS obtendrá el valor real con impuestos")}
+   else {
+    alert("Este servicio es solo para residentes de Argentina")
+        }
+
+
+
