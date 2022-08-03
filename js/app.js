b06ce8fd6433c8f940abe9538850b38c8d628f05
Rodrigo <rodrigo.ezequiel.ruiz@gmail.com>
2022-08-03 17:20:45 -0300

    descarga en la notebook

--- js/app.js
+++ js/app.js
@@ -1,18 +1,43 @@
 
-let nameMovies = ['Lord of the rings', 'Scary Movie', 'Pineapple Express', 'Jack Ass 2']
+ let nameMovies = []
+
 
 alert("Bienvenido al listado de peliculas favoritas")
-
+if (nameMovies.length === 0){
+   alert("¡Aún no tenés ninguna película agregada!")
+} else{
 for (let i = 0; i < nameMovies.length; i++) {
    alert(nameMovies[i]);
-}
+}}
 
 function addMovies(movie){
- nameMovies.push(movie)
-alert("Listado de tus peliculas favoritas")
- for (let i = 0; i < nameMovies.length; i++) {
-    alert(nameMovies[i]);
- } }
- addMovies(prompt("Podes agregarle una pelicula nueva")) 
+   if (movie === ""){
+        alert("Ingresá una pelí")
+        addMovies(prompt("Elegi una película valida (no espacios vacios)")) 
+   } else{
+nameMovies.push(movie)
+if (confirm('¿Querés agregar otra péli?')) {
+   addMovies(prompt("Podes agregarle una pelicula nueva"))} }}
+     
+         
+      
 
+
+while(true){
+ addMovies(prompt("Podes agregarle una pelicula nueva")) 
+ alert("Mostrando listado de tus peliculas favoritas")
+   if (nameMovies.length == 0){
+      alert("¡Aún no tenés ninguna película agregada!")
+   } else{
+   for (let i = 0; i < nameMovies.length; i++) {
+      alert(nameMovies[i]);
+   }}}
+
+
+
+
+ 
+
+
+ 
 
