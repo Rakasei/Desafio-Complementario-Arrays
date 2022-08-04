3541211a091283f76e9d1d028161b27798fc9329
Rodrigo <rodrigo.ezequiel.ruiz@gmail.com>
2022-08-03 21:33:45 -0300

    descarga en la notebook

--- js/app.js
+++ js/app.js
@@ -1,37 +1,34 @@
 
- let nameMovies = []
+ let nombresPeliculas= []
 
 
 alert("Bienvenido al listado de peliculas favoritas")
-if (nameMovies.length === 0){
+if (nombresPeliculas.length === 0){
    alert("¡Aún no tenés ninguna película agregada!")
 } else{
-for (let i = 0; i < nameMovies.length; i++) {
-   alert(nameMovies[i]);
+for (let i = 0; i < nombresPeliculas.length; i++) {
+   alert(nombresPeliculas[i]);
 }}
 
-function addMovies(movie){
-   if (movie === ""){
+function agregarPeliculas(pelicula){
+   if (pelicula === ""){
         alert("Ingresá una pelí")
-        addMovies(prompt("Elegi una película valida (no espacios vacios)")) 
+        agregarPeliculas(prompt("Elegi una película valida (no espacios vacios)")) 
    } else{
-nameMovies.push(movie)
+nombresPeliculas.push(pelicula)
 if (confirm('¿Querés agregar otra péli?')) {
-   addMovies(prompt("Podes agregarle una pelicula nueva"))} }}
-     
-         
-      
-
-
+   agregarPeliculas(prompt("Podes agregarle una pelicula nueva"))} }}
+                
 while(true){
- addMovies(prompt("Podes agregarle una pelicula nueva")) 
+ agregarPeliculas(prompt("Podes agregarle una pelicula nueva")) 
  alert("Mostrando listado de tus peliculas favoritas")
-   if (nameMovies.length == 0){
+   if (nombresPeliculas.length == 0){
       alert("¡Aún no tenés ninguna película agregada!")
    } else{
-   for (let i = 0; i < nameMovies.length; i++) {
-      alert(nameMovies[i]);
+   for (let i = 0; i < nombresPeliculas.length; i++) {
+      alert(nombresPeliculas[i]);
    }}}
+
 
 
 
